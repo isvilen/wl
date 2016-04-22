22ce0f026fd909c3a6b8814e3e891553a6d68f70
Svilen Ivanov <isvilen@applicata.bg>
2016-04-22 23:57:30 +0300

    Add wl:find / wl_registry_handler:find methods

--- src/wl.erl
+++ src/wl.erl
@@ -5,6 +5,7 @@
         , disconnect/1
         , bind/2
         , bind/3
+        , find/2
         ]).
 
 connect() ->
@@ -14,10 +15,10 @@ connect() ->
 connect(SocketPath) ->
     case wl_connection:start_link(SocketPath, wl_display_handler) of
         {ok, Conn} ->
-            % make wl_registry singleton object available
+            % make wl_registry singleton object available with id 2
             Display = wl_connection:display(Conn),
             _ = wl_display:get_registry(Display, wl_registry_handler),
-            case wl_display_handler:sync(Display, 5000) of
+            case wl_display_handler:sync(Display) of
                 ok      -> {ok, Conn};
                 timeout -> wl_connection:stop(Conn), {error, timeout}
             end;
@@ -34,9 +35,29 @@ bind(Conn, Itf) ->
     bind(Conn, Itf, wl_default_handler).
 
 
+bind(Conn, Itf, Pid) when is_pid(Pid) ->
+    bind(Conn, Itf, {wl_default_handler, Pid});
+
 bind(Conn, Itf, Handler) ->
-    Registry = wl_connection:id_to_pid(Conn ,2),
-    wl_registry_handler:bind(Registry, Itf, Handler).
+    Registry = wl_connection:id_to_pid(Conn, 2),
+    case wl_registry_handler:bind(Registry, Itf, Handler) of
+        {ok, Result}    -> Result;
+        {error, Reason} -> error(Reason)
+    end.
+
+
+find(Conn, wl_display) ->
+    wl_connection:display(Conn);
+
+find(Conn, wl_registry) ->
+    wl_connection:id_to_pid(Conn ,2);
+
+find(Conn, Itf) ->
+    Registry = wl_connection:id_to_pid(Conn, 2),
+    case wl_registry_handler:find(Registry, Itf) of
+        {ok, Result}    -> Result;
+        {error, Reason} -> error(Reason)
+    end.
 
 
 %% Internal functions
--- src/wl_registry_handler.erl
+++ src/wl_registry_handler.erl
@@ -1,6 +1,7 @@
 -module(wl_registry_handler).
 -export([ globals/1
         , bind/3
+        , find/2
         , init/2
         , handle_event/3
         , handle_call/2
@@ -12,41 +13,68 @@ globals(Registry) ->
 
 
 bind(Registry, Itf, Handler) ->
-    case wl_object:call(Registry, {globals, Itf}) of
+    wl_object:call(Registry, {bind, Itf, Handler}).
+
+
+find(Registry, Itf) ->
+    wl_object:call(Registry, {find, Itf}).
+
+
+-record(state,{globals,bindings}).
+
+init(_Parent, _ItfVer) ->
+    {ok, #state{globals=#{},bindings=#{}}}.
+
+
+handle_event(global, [Name, ItfStr, Ver], #state{globals=Globals}=State) ->
+    Itf = list_to_atom(binary_to_list(ItfStr)),
+    {new_state, State#state{globals=maps:put(Name, {Itf, Ver}, Globals)}};
+
+handle_event(global_remove, [Name], #state{globals=Globals}=State) ->
+    {new_state, State#state{globals=maps:remove(Name, Globals)}}.
+
+
+handle_call(globals, #state{globals=Globals}) ->
+    {reply, Globals};
+
+handle_call({bind, Itf, Handler}, State) ->
+    case find_globals(State#state.globals, Itf) of
         [] ->
-            not_supported;
+            {reply, {error, {not_supported, Itf}}};
         [NameVer] ->
-            bind_1(Registry, Itf, NameVer, Handler);
+            Pid = do_bind(Itf, NameVer, Handler),
+            NewBindings = register_binding(Itf, Pid, State#state.bindings),
+            {reply, {ok, Pid}, State#state{bindings=NewBindings}};
         Values ->
-            [bind_1(Registry, Itf, NameVer, Handler) || NameVer <- Values]
+            Pids = [do_bind(Itf, NameVer, Handler) || NameVer <- Values],
+            NewBindings = register_binding(Itf, Pids, State#state.bindings),
+            {reply, {ok, Pids, State#state{bindings=NewBindings}}}
+    end;
+
+handle_call({find, Itf}, #state{bindings=Bindings}) ->
+    case maps:get(Itf, Bindings, undefined) of
+        undefined -> {reply, {error, not_binded}};
+        [Pid]     -> {reply, {ok, Pid}};
+        Pids      -> {reply, {ok, Pids}}
     end.
 
 
-bind_1(Registry, Itf, NameVer, Handler) when is_pid(Handler) ->
-    bind_1(Registry, Itf, NameVer, {wl_default_handler, Handler});
-
-bind_1(Registry, Itf, {Name, Ver}, Handler) ->
-    V = min(Itf:interface_info(version), Ver),
-    wl_registry:bind(Registry, Name, {Itf, V, Handler}).
-
-
-init(_Parent, _ItfVer) ->
-    {ok, #{}}.
-
-
-handle_event(global, [Name, ItfStr, Ver], Globals) ->
-    Itf = list_to_atom(binary_to_list(ItfStr)),
-    {new_state, maps:put(Name, {Itf, Ver}, Globals)};
-
-handle_event(global_remove, [Name], Globals) ->
-    {new_state, maps:remove(Name, Globals)}.
-
-
-handle_call(globals, Globals) ->
-    {reply, Globals};
-
-handle_call({globals, Itf}, Globals) ->
+find_globals(Globals, Itf) ->
     Fun = fun (K, {I, V}, Acc) when I == Itf -> [{K, V} | Acc];
               (_, _, Acc) -> Acc
           end,
-    {reply, maps:fold(Fun, [], Globals)}.
+    maps:fold(Fun, [], Globals).
+
+
+do_bind(Itf, {Name, Ver}, Handler) ->
+    V = min(Itf:interface_info(version), Ver),
+    wl_registry:bind(self(), Name, {Itf, V, Handler}).
+
+
+register_binding(Itf, PidOrPids, Bindings) ->
+    Values = maps:get(Itf, Bindings, []),
+    NewValues = if
+                    is_list(PidOrPids) -> PidOrPids ++ Values;
+                    true               -> [PidOrPids | Values]
+                end,
+    maps:put(Itf, NewValues, Bindings).
